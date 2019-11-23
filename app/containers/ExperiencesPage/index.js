b39bc2df3785aa1f5626e64f427d62f34beab48d
Junior Campos <laccuvit@gmail.com>
2019-11-23 10:55:31 -0300

    a

--- app/containers/ExperiencesPage/index.js
+++ app/containers/ExperiencesPage/index.js
@@ -9,13 +9,45 @@ const Root = styled.div`
   height: 100%;
   overflow: scroll;
   padding: 10px;
+  display: grid;
+  grid-template-columns: 150px auto;
+  grid-template-rows: 150px auto;
+  grid-template-areas:
+    'header content'
+    'sidebar content';
+`;
+
+const Blue = styled.div`
+  background-color: blue;
+  width: 100%;
+  height: 100%;
+`;
+
+const Red = styled.div`
+  background-color: red;
+  width: 100%;
+  height: 100%;
+  grid-row-end: span 2;
+`;
+
+const Green = styled.div`
+  background-color: green;
+  width: 100%;
+  height: 100%;
+`;
+
+const Purple = styled.div`
+  background-color: purple;
+  width: 100%;
+  height: 100%;
 `;
 
 export default function ExperiencesPage() {
   return (
     <Root>
-      <img src={stone} />
-      <Button>Stone Pagamentos</Button>
+      <Green className="header">green</Green>
+      <Red className="content">red</Red>
+      <Blue className="sidebar">blue</Blue>
     </Root>
   );
 }
